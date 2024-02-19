062660800aa803dfaef64c02eba1d9d1ca04e173
mansur3383 <140749343+mansur3383@users.noreply.github.com>
2024-02-19 20:20:07 +0300

    Create WriterToFile.java

---
+++ WriterToFile.java
@@ -0,0 +1,38 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriterToFile {
+
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public void InputWriter(String[] values, boolean indexValue, boolean indexLength){
+        StringToValue StringToValue = new StringToValue();
+
+        if (indexValue && indexLength) {
+            try {
+                FileWriter writer;
+                String data;
+
+                try {
+
+                    // Запись данных в строку
+                    data = "<" + values[0] + "><" + values[1] + "><"
+                            + values[2] + "><" + values[3] + "><"
+                            + values[4] + "><" + values[5] + ">\n";
+
+                    // Запись данных в файл
+                    writer = new FileWriter("lesson3/src/notes/" + values[0] + "_note.txt", true);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                writer.write(data); // <- запись строки data в файл surname_note.txt
+                writer.close();
+                System.out.println(ANSI_CYAN + "значение успешно записано в файл" + ANSI_RESET);
+            } catch (IOException e) {
+                System.out.println("ошибка при записи в файл");
+            }
+        }
+    }
+}
