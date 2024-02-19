427c67d82488300a356b53422f70d08613115110
mansur3383 <140749343+mansur3383@users.noreply.github.com>
2024-02-19 20:20:53 +0300

    Create StringToValue.java

---
+++ StringToValue.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class StringToValue {
+
+    public static boolean calculateIndexValue() {
+        return true;
+    }
+    public void ValueStrings(String[] values){
+
+        // Преобразование значений к нужным типам
+        String surname = values[0];
+        String name = values[1];
+        String patronymic = values[2];
+        String dateOfBirth = values[3];
+        String phoneNumber = values[4];
+        char gender = values[5].charAt(0);
+
+        // Проверка форматов данных
+        if (!surname.matches("[а-яА-Яa-zA-Z]+") || !name.matches("[а-яА-Яa-zA-Z]+") || !patronymic.matches("[а-яА-Яa-zA-Z]+")) {
+            throw new IllegalArgumentException("Фамилия, имя и отчество должны состоять только из букв");
+        }
+        if (!phoneNumber.matches("[0-9]+")) {
+            throw new IllegalArgumentException("номер телефона должен состоять только из цифр");
+        }
+        if (gender != 'm' && gender != 'f') {
+            throw new IllegalArgumentException("неверное значение пола");
+        }
+
+        // Проверка и парсинг даты рождения
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate birthDate = LocalDate.parse(dateOfBirth, dtf);
+
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("\"некорректный формат даты рождения\"");
+        }
+    }
+}
